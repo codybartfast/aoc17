91125496123faefc0d807d26aa4cdd69134949fe
John Donnellan <john@blueTribu.com>
2017-12-17 17:21:56 +0000

    Day 1

---
+++ Program.fs
@@ -0,0 +1,40 @@
+﻿open System
+
+// Day 1
+let inverseCaptureA input = 
+    let chars = Seq.toArray input   
+    [ chars; [| chars.[0] |] ]
+    |> Seq.concat
+    |> Seq.pairwise
+    |> Seq.filter (fun (a, b) -> a = b)
+    |> Seq.map fst
+    |> Seq.map Char.GetNumericValue
+    |> Seq.sum
+    |> string
+
+let inverseCaptureB input = 
+    let chars = Seq.toArray input    
+    let halfLen = chars.Length / 2
+    chars
+    |> Seq.take halfLen
+    |> Seq.mapi (fun i c -> (c, chars.[i + halfLen]))
+    |> Seq.filter (fun (a, b) -> a = b)
+    |> Seq.map fst
+    |> Seq.map Char.GetNumericValue
+    |> Seq.sum
+    |> (*) 2.0
+    |> string
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [| "1a"; input |] -> inverseCaptureA input
+    | [| "1b"; input |] -> inverseCaptureB input
+    | _ -> "Merry Christmas from F#!"
+    |> printfn "%s"
+    Console.ReadLine() |> ignore
+    0
+
+
+
+
